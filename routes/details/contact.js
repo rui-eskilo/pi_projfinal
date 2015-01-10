2819d965ad9fa65988acba6ed06b2caaad43eec0
vicmorais <28499@alunos.isel.ipl.pt>
2015-01-10 22:56:08 +0000

    nodemon install. Login page, register page, about page, contact page created.

--- routes/details/contact.js
+++ routes/details/contact.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.render('details/about');
+  res.render('details/contact');
 });
 
 module.exports = router;
