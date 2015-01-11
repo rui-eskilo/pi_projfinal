8f4a4ac706a0af7051c250367065df08ff9e2200
vicmorais <28499@alunos.isel.ipl.pt>
2015-01-11 01:43:57 +0000

    sendEmail example with a ajax request working

---
+++ public/javascripts/mail.js
@@ -0,0 +1,23 @@
+var sendMail = function() {
+    $.ajax({
+      type: 'POST',
+      url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+      data: {
+        'key': 'nWryQ-a-tKIYoicHqHPmWw',
+        'message': {
+          'from_email': 'victor.silva.morais@gmail.com',
+          'to': [
+              {
+                'email': 'victor.silva.morais@gmail.com',
+                'type': 'to'
+              }
+            ],
+          'autotext': 'true',
+          'subject': 'YOUR SUBJECT HERE!',
+          'html': 'YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!'
+        }
+      }
+     }).done(function(response) {
+       console.log(response); // if you're into that sorta thing
+     });
+};
