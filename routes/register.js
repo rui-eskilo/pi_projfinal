e475940b5b8065ca7e4b625d29099ad984e18361
vicmorais <28499@alunos.isel.ipl.pt>
2015-02-05 23:11:05 +0000

    Register, done

--- routes/register.js
+++ routes/register.js
@@ -1,5 +1,15 @@
+var userDB = require('./../db/user');
+var express = require('express');
+
 module.exports = function(app) {
-	app.get('/register', function(req, res) {
+	app.get('/register', function(req, res, next) {
   		return res.render('register');
+	});
+
+	app.post('/register', function(req, res, next) {
+  		userDB.create(req.body.username, req.body.password, req.body.nickname, req.body.email, 'UE', function(err, result){
+  		if(err) return next(err);
+  		return res.redirect('/login');
+  		});
 	});
 }
