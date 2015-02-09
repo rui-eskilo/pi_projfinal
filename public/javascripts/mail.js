3e385f0a84d32d682867357d4a3340b741432446
vicmorais <28499@alunos.isel.ipl.pt>
2015-02-09 20:08:30 +0000

    Contact view finished. And working with Ajax and DOM

--- public/javascripts/mail.js
+++ public/javascripts/mail.js
@@ -1,13 +1,16 @@
 
-var sendMail = function(name, email, coment) {
-	console.log(name);
+var sendMail = function() {
+	 var name = document.getElementById("name");
+   var mail = document.getElementById("mail");
+   var comment = document.getElementById("comment");
+   var div = document.getElementById("contact");
     $.ajax({
       type: 'POST',
       url: 'https://mandrillapp.com/api/1.0/messages/send.json',
       data: {
         'key': 'nWryQ-a-tKIYoicHqHPmWw',
         'message': {
-          'from_email': 'victor.silva.morais@gmail.com',
+          'from_email': mail.value,
           'to': [
               {
                 'email': 'victor.silva.morais@gmail.com',
@@ -15,11 +18,15 @@ var sendMail = function(name, email, coment) {
               }
             ],
           'autotext': 'true',
-          'subject': 'YOUR SUBJECT HERE!',
-          'html': 'YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!'
+          'subject': 'Nome: ' + name.value,
+          'html': comment.value
         }
       }
      }).done(function(response) {
-       alert('Enviado');
+      div.removeChild(document.getElementById("contactForm"));
+      var h4 = document.createElement("h4");
+      var text = document.createTextNode("A tua mensagem foi enviada com sucesso. Certamente iremos responder num futuro próximo.\n No entanto se isso não acontecer podes sempre registar-te e apresentar uma queixinha! (\",)");
+      div.appendChild(h4.appendChild(text));
      });
+     return false;
 };
