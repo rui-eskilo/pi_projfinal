1feb0a300c54c3043f15ca7b1f9f20a0ba7e90f9
vicmorais <28499@alunos.isel.ipl.pt>
2015-02-09 23:14:10 +0000

    Forgot_password working and sending email.

--- routes/user/forgot_password.js
+++ routes/user/forgot_password.js
@@ -2,6 +2,10 @@ var userDB = require('./../../db/user');
 var userDB = require('./../../db/user');
 var express = require('express');
 
+var request = require('request');
+
+
+
 module.exports = function(app) {
 	var details = {};
 	details.state = 0;
@@ -11,19 +15,38 @@ module.exports = function(app) {
 
 	app.post('/forgot_password', function(req, res, next) {
 
-		
-
 		userDB.findUserByEmail(req.body.email, function(err, user) {
 			if(err) return next(err);
 			if(!user) {
 				details.state = 1;
 				return res.render('./user/forgot_password', {details : details});
 			} else {
-				details.state = 2;
-				console.log(user);
-				details.username = user.username;
-				details.password = user.password;
-				return res.render('./user/forgot_password', {details : details});
+				request.post(
+    				'https://mandrillapp.com/api/1.0/messages/send.json',
+   					 { 
+   					 	form: {
+        					'key': 'nWryQ-a-tKIYoicHqHPmWw',
+        					'message': {
+          					'from_email': 'victor.silva.morais@gmail.com',
+          					'to': [
+              					{'email': user.email,
+                				'type': 'to' }],
+          					'autotext': 'true',
+          					'subject': 'Recuperação de Password e Username' ,
+          					'html': 'O seu Username é:' + user.username + ' e a password: ' + user.password
+        					}
+      				}},
+					    function (error, response, body) {
+					    	console.log(body);
+					        if (!error && response.statusCode == 200) {
+					            details.state = 2;
+					            return res.render('./user/forgot_password', {details : details});
+					        } else {
+					        	details.state = 3;
+					        	return res.render('./user/forgot_password', {details : details});
+					        }
+					    }
+					);
 			}
 		});
 	});
